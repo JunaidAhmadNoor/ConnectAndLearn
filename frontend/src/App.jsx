e1a0f7d7642f5d2aec91ebeba68bf4bc60b1c0df
JunaidAhmadNoor <imjunaidnoor@gmail.com>
2024-04-24 16:33:26 +0500

    landingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/home';
+import LandingPage from './components/landingpage';
 import Signin from './components/signin';
 import Signup from './components/signup';
 
@@ -9,7 +9,7 @@ const App = () => {
         <Router>
             <div>
                 <Routes>
-                    <Route path="/" exact element={<Home />} />
+                    <Route path="/" exact element={<LandingPage />} />
                     <Route path="/signin" element={<Signin />} />
                     <Route path="/signup" element={<Signup />} />
                 </Routes>
--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="bg-white p-3 w-full fixed top-0">
+        <nav className={` p-3 w-full fixed top-0 z-50 ${isScrolled ? 'ml-14 mt-4 w-11/12 rounded-full bg-gray-300' : ''}`}>
             <div className="container mx-auto flex justify-between items-center">
                 {/* Left side: Logo */}
                 <div className="flex items-center">
-                    <Link to="/" className="text-white text-xl font-bold flex items-center">
-                        <img src="/logo.png" alt="Logo" className=" px-4 h-8 w-auto mr-2" />
+                    <Link to="/" className="text-black text-xl font-bold flex items-center">
+                        <img src="/logo.png" alt="Logo" className="h-8 w-auto mr-2" />
                     </Link>
                 </div>
 
                 {/* Right side: Signin and Signup */}
                 <div className="flex items-center">
-                    <Link to="/signin" className="px-4 text-black mr-4">
+                    <Link to="/signin" className="pr-2 text-black mr-4">
                         Signin
                     </Link>
-                    <Link to="/signup" className="px-4 text-black">
+                    <Link to="/signup" className="pr-10 text-black">
                         Signup
                     </Link>
                 </div>
